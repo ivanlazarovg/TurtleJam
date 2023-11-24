11f62e6195fc10018bbac88a9e0ff2b17842c607
Kris-Dimitrov <k31122004@gmail.com>
2023-11-25 00:16:23 +0100

    BugFix - enemies now do consistent damage to player

--- Assets/Scripts/EnemyChaserScript.cs
+++ Assets/Scripts/EnemyChaserScript.cs
@@ -23,7 +23,7 @@ public class EnemyChaserScript : MonoBehaviour
         hitTimer -= Time.deltaTime;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player") && hitTimer <= 0)
         {
