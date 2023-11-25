14d6eb2b2a9e6e0a5024feb5091b1b76e237dcd5
Kris-Dimitrov <k31122004@gmail.com>
2023-11-25 01:17:31 +0100

    Added score, fixed collisions between player and player projectiles

--- Assets/Scripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemyHealthScript : MonoBehaviour
 {
-    [SerializeField] private int health;
+    private int health;
+    [SerializeField] private int maxHealth;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
 
     public void TakeDamage(int damage) 
     {
         health -= damage;
         Debug.Log(health);
 
-        if (health <= 0) 
+        if (health <= 0)
         {
+            FindFirstObjectByType<ScoreManagerScript>().AddScore(maxHealth);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerScript : MonoBehaviour
+{
+    [SerializeField] private int score;
+
+    public void AddScore(int add) 
+    {
+        score += add;
+        UpdateInfo();
+    }
+
+    private void UpdateInfo() 
+    {
+        // add ui
+        Debug.Log("Score: " + score);
+    }
+}
