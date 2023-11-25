11c3bec54066dec2c8abedc4650ca8deaa802997
Ivan Lazarov <43958429+ivanlazarovg@users.noreply.github.com>
2023-11-25 07:25:52 +0100

    commitment issues

--- Assets/Scripts/TaskCrypto.cs
+++ Assets/Scripts/TaskCrypto.cs
@@ -7,11 +7,20 @@ public class TaskCrypto : MonoBehaviour
     public Transform pointer;
     public Transform leftPoint;
     public Transform rightPoint;
+    public Transform centerPoint;
+    public GameObject window;
     float timer = 0;
     bool pingpongswitch = false;
+    public float pointerSpeed;
+    public float pointerSpeedStartState;
+    [Range(0, 1)]
+    public float pointDifficulty;
     public bool isInWindow = false;
 
     int interactionPhase = 1;
+
+    float pointerDistanceFromCenter;
+    float pointerDistanceFromCenterRaw;
 
     private static TaskCrypto _instance;
 
@@ -35,30 +44,35 @@ public class TaskCrypto : MonoBehaviour
         {
             StartState();
         }
-        
-    }
-
-    void StartState()
-    {
+        else if(interactionPhase == 2)
+        {
+            MineState();
+        }
         if (!pingpongswitch)
         {
-            timer += Time.deltaTime;
+            timer += pointerSpeed * Time.deltaTime;
             if (timer >= 1)
             {
                 pingpongswitch = true;
+                //pointerSpeed = Random.Range(1.2f, 2.5f);
             }
         }
         else if (pingpongswitch)
         {
-            timer -= Time.deltaTime;
+            timer -= pointerSpeed * Time.deltaTime;
             if (timer <= 0)
             {
                 pingpongswitch = false;
+                //pointerSpeed = Random.Range(1.2f, 2.5f);
             }
         }
 
         pointer.position = Vector2.Lerp(leftPoint.position, rightPoint.position, timer);
 
+    }
+
+    void StartState()
+    { 
         if (Input.GetMouseButtonDown(0) && isInWindow)
         {
             Debug.Log("Started");
@@ -67,6 +81,28 @@ public class TaskCrypto : MonoBehaviour
         else if (Input.GetMouseButtonDown(0) && !isInWindow)
         {
             Debug.Log("Missed");
+        }
+        pointerSpeed = pointerSpeedStartState;
+        window.SetActive(true);
+    }
+
+    void MineState()
+    {
+        window.SetActive(false);
+        if(Mathf.Abs(pointer.position.x - leftPoint.position.x) > Mathf.Abs(pointer.position.x - rightPoint.position.x))
+        {
+            pointerDistanceFromCenterRaw = Mathf.Abs(pointer.position.x - rightPoint.position.x);
+        }
+        else
+        {
+            pointerDistanceFromCenterRaw = Mathf.Abs(pointer.position.x - leftPoint.position.x);
+        }
+        pointerDistanceFromCenter = Mathf.Round(pointerDistanceFromCenterRaw/(3+pointDifficulty));
+        pointerSpeed = 1.3f + pointerDistanceFromCenterRaw / 3;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(pointerDistanceFromCenter);
         }
     }
 
