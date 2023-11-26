83633bd2255b0c82a3abe2173d3d3a356dd3bf61
Ivan Lazarov <43958429+ivanlazarovg@users.noreply.github.com>
2023-11-26 15:06:24 +0100

    killen me

--- Assets/Scripts/ShiftBetweenTasks.cs
+++ Assets/Scripts/ShiftBetweenTasks.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShiftBetweenTasks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float timerToShift;
+    public Task[] tasks;
+    public TextMeshProUGUI countdown;
+    Task currentPick;
+    Task lastPick;
+    public float timeMod;
+
+    private void Start()
     {
-        
+        tasks = Resources.FindObjectsOfTypeAll<Task>();
+        Shift();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(timerToShift <= 0f)
+        {
+            Shift();      
+        }
+        timerToShift -= Time.deltaTime;
+        //countdown.text = Mathf.Round(timerToShift).ToString();
+    }
+
+    void Shift()
+    {
+        foreach (Task task in tasks)
+        {
+            task.gameObject.SetActive(false);
+        }
+
+        tasks[Random.Range(0,3)].gameObject.SetActive(true);
         
+        timerToShift = timeMod;
     }
 }
